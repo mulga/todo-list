908a9fe735023c9c61f0ab2418b755fb08a55d0a
Mehmet Ulga <mehmetulga@gmail.com>
2022-04-17 14:44:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,11 @@ function addItem() {
     });
   }
 }
+
+//  Trigger a Button Click on Enter
+userInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("addToDo").click();
+  }
+});
